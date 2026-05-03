94f245ff323903cacb0177f858afdc9c2584c9cb
NikilSrivatsa <iam.nikil97@gmail.com>
2026-05-03 18:25:46 +0530

    Delete automated-blue-green-deployment-strategy-using directory

--- automated-blue-green-deployment-strategy-using/scripts/current-color.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-NAMESPACE="${NAMESPACE:-blue-green-demo}"
-APP_NAME="${APP_NAME:-blue-green-demo}"
-
-kubectl -n "$NAMESPACE" get service "$APP_NAME" -o jsonpath='{.spec.selector.color}'
-printf '\n'
